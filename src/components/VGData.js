2f0504c41244eddd42681dc685d4fb28f0b02b17
MaxiAlem <maerc.maxi@gmail.com>
2022-08-04 16:49:25 -0300

    test (maybe)solved metatags meta tags

--- src/components/VGData.js
+++ src/components/VGData.js
@@ -1,41 +1,73 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState,useEffect,useLayoutEffect } from 'react'
 import { consult } from '../helpers/api'
 
 
 
   let data = {}
 const VGData = () => {
+  
   const [game, setGame] =useState({name:'',background_image:'', released:'',dominant_color:''})
   const [loading, setLoading]= useState(true)
-  const onClick = async()=>{
+  const takeOne = async()=>{
     try {
     const res = await consult()
     data = await res
-    setGame(data)
+    setGame(data)  
+    setLoading(false)
     
-    setLoading(false) 
     } catch (error) {
     console.log(error)
     }
-    
     
   }
     const urlYT =`https://www.youtube.com/results?search_query=${game.name.replaceAll(' ','+')}+walkthrough`
     const img =()=>{
       if(game.background_image === null){
-        return 'no_img.webp'
+        return 'no_img.jpg'
       }else {
         return game.background_image}
+      
     }
+    const  metaTags=()=>{
+      const metaogs = document.getElementsByTagName('meta')
+      console.log(metaogs[3])
+
+      metaogs[3].setAttribute('content', game.name) //title
+      metaogs[5].setAttribute('content', img())//image
+      metaogs[6].setAttribute('content', img())//image:secure_url
+      metaogs[4].setAttribute('content', `A Day Like Today but in  ${game.released.slice(0,4)}, ${game.name} was released`)
+      //  const ogTitle = document.createElement('meta');
+  //  ogTitle.setAttribute('property', 'og:type');
+  //  ogTitle.setAttribute('content', game.name);
+
+  //  const ogImg = document.createElement('meta');
+  //  ogImg.setAttribute('property', 'og:image');
+  //  ogImg.setAttribute('content', img());
+   
+  //  const ogdescr = document.createElement('meta');
+  //  ogdescr.setAttribute('property', 'og:description');
+  //  ogdescr.setAttribute('content', `A Day Like Today but in  ${game.released.slice(0,4)}, ${game.name} was released`);
+
+  //  document.head.prepend(ogImg)
+  //  document.head.prepend(ogTitle)
+   console.log(game.name)
+   
+   }
+    
+   
+
     useEffect(() => {
       
-    
-     
-        onClick()
+      takeOne();
       
-    },[]);
-    
+    },[])// eslint-disable-line react-hooks/exhaustive-deps
+    useLayoutEffect(() => {
+  
+      return metaTags()
+     
+    }, [game])
   return (
+    <>
     <div className='VGData'>
       {loading ? <div className='div-load'>loading ...</div>
                 
@@ -55,11 +87,13 @@ const VGData = () => {
       <div className='div-btn'>
         <button
           className="nes-btn is-error"
-          onClick={onClick}>
+          onClick={takeOne}>
         <div>try again</div>
         </button>
       </div>  
     </div>
+    </>
+    
   )
 }
 
